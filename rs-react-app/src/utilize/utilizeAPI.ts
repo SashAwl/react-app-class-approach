df2a15e5ee7fe95da251e78831913284cf5bcf81
Alexandra Zaporenko <aleks.shilova@gmail.com>
2025-07-15 15:52:27 +0300

    refactor: extract data fetching logic to a separate module

---
+++ rs-react-app/src/utilize/utilizeAPI.ts
@@ -0,0 +1,33 @@
+import type { Character } from '../types';
+
+const BASE_URL = 'https://rickandmortyapi.com/api/character';
+
+function getRequestURL(query: string) {
+  let queryString = `${BASE_URL}/?page=1`;
+  queryString += query ? `&name=${query}` : '';
+  return queryString;
+}
+
+export async function fetchCharacters(
+  query: string,
+  onSuccess: (characters: Character[]) => void,
+  onError: (message: string) => void
+) {
+  try {
+    const response = await fetch(getRequestURL(query));
+    const dataJSON = await response.json();
+
+    if (!response.ok) {
+      if (response.status === 404) {
+        throw new Error('No characters found for your query');
+      } else {
+        throw new Error(`Server error: ${response.status}`);
+      }
+    }
+
+    onSuccess(dataJSON.results);
+  } catch (error) {
+    console.log(error);
+    onError('No characters found for your query');
+  }
+}
--- rs-react-app/src/utilize/utilizeLocalStorage.ts
+++ rs-react-app/src/utilize/utilizeLocalStorage.ts
@@ -1,13 +1,3 @@
-const BASE_URL = 'https://rickandmortyapi.com/api/character';
-
-export function getRequestURL(query: string) {
-  let queryString = `${BASE_URL}/?page=1`;
-  queryString += query ? `&name=${query}` : '';
-  return queryString;
-}
-
-/** LocalStorage operation */
-
 export function initialLocalStorage(): void {
   if (!localStorage.getItem('quueryTerm')) {
     localStorage.setItem('quueryTerm', '');
