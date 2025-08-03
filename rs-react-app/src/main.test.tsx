31eebbbb29865015bfe3a580af4230fe37404783
Alexandra Zaporenko <aleks.shilova@gmail.com>
2025-08-04 02:04:22 +0300

    fix: correct routing

--- rs-react-app/src/main.test.tsx
+++ rs-react-app/src/main.test.tsx
@@ -1,18 +1,35 @@
 import { afterEach, describe, expect, test } from 'vitest';
+import { screen } from '@testing-library/react';
 import { renderApp } from './main';
 import { shouldRenderApp } from './main';
+import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { App } from './App/App';
 
 describe('Tests main.tsx', () => {
   afterEach(() => {
     document.body.innerHTML = '';
   });
 
-  test('Renders without crashing when #root exists', () => {
+  test('Renders without crashing when #root exists', async () => {
     const rootDiv = document.createElement('div');
     rootDiv.setAttribute('id', 'root');
     document.body.appendChild(rootDiv);
 
-    expect(() => renderApp()).not.toThrow();
+    const root = createRoot(rootDiv);
+
+    root.render(
+      <StrictMode>
+        <BrowserRouter basename="react-app-class-approach">
+          <App />
+        </BrowserRouter>
+      </StrictMode>
+    );
+
+    await screen.findByText(/your results/i);
+
+    root.unmount();
   });
 
   test('Throws error when #root does not exist', () => {
