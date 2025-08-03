26fc3bb2a12f489b60df190897e73f9861de6f83
Alexandra Zaporenko <aleks.shilova@gmail.com>
2025-08-03 23:20:41 +0300

    fix: correct routing error

--- rs-react-app/src/App/App.tsx
+++ rs-react-app/src/App/App.tsx
@@ -2,8 +2,19 @@ import './App.css';
 import { Footer } from '../components/Footer/Footer';
 import { Header } from '../components/Header/Header';
 import { Main } from '../components/Main/Main';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const redirect = sessionStorage.redirect;
+    if (redirect) {
+      sessionStorage.removeItem('redirect');
+      navigate(redirect);
+    }
+  }, []);
   return (
     <div className="bg-olive-gradient">
       <Header />
